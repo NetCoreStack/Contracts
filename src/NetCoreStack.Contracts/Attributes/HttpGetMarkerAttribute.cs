eb4682cdc1f13fe657cfb91e1cc5b8dbbbd5043c
Gencebay <gencebay.demir@gmail.com>
2017-05-07 12:24:36 +0300

    HttpGetMarkerAttribute added

---
+++ src/NetCoreStack.Contracts/Attributes/HttpGetMarkerAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NetCoreStack.Contracts
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = false)]
+    public sealed class HttpGetMarkerAttribute : HttpMethodMarkerAttribute
+    {
+        public HttpGetMarkerAttribute()
+        {
+        }
+    }
+}
