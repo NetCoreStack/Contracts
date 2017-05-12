2385eaaa0d7def2991a7a127402bfe50632ce6a5
Gencebay <gencebay.demir@gmail.com>
2017-05-12 11:06:07 +0300

    Data contracts

---
+++ src/NetCoreStack.Data.Contracts/EntityIdentitySql.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using NetCoreStack.Contracts;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NetCoreStack.Data.Contracts
+{
+    public class EntityIdentitySql : SqlEntityBase, IEntityIdentity<long>
+    {
+        public long Id { get; set; }
+
+        [NotMapped]
+        public ObjectState ObjectState { get; set; }
+    }
+}
---
+++ src/NetCoreStack.Data.Contracts/SqlEntityBase.cs
@@ -0,0 +1,11 @@
+﻿using NetCoreStack.Contracts;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NetCoreStack.Data.Contracts
+{
+    public abstract class SqlEntityBase : IEntity
+    {
+        [NotMapped]
+        public ObjectState ObjectState { get; set; }
+    }
+}
