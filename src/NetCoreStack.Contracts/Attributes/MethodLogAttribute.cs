37df83d01340cb60d5c876702f90e657e1dbcac9
gencebay <gencebay.demir@gmail.com>
2018-02-11 01:32:02 +0300

    method log attribute, service exception type added

---
+++ src/NetCoreStack.Contracts/Attributes/MethodLogAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace NetCoreStack.Contracts
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+    public sealed class MethodLogAttribute : Attribute
+    {
+        public MethodLogAttribute()
+        {
+
+        }
+    }
+}
