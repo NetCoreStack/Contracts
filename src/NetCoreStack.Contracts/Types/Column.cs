0fd9b42b58da4d29787abb4ee5d7234645191022
gencebay <gencebay.demir@gmail.com>
2019-10-15 01:23:44 +0300

    Column original Prop Name prop

--- src/NetCoreStack.Contracts/Types/Column.cs
+++ src/NetCoreStack.Contracts/Types/Column.cs
@@ -2,16 +2,12 @@
 {
     public class Column
     {
+        public string Name { get; set; }
         public string Data { get; set; }
-
         public string Meta { get; set; }
-
         public string Composer { get; set; }
-
         public bool Searchable { get; set; }
-
         public bool Orderable { get; set; }
-
         public Search Search { get; set; }
     }
 }
